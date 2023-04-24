6f880f23af68a345428883fa4515862e91cb50ac
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2023-04-24 14:41:01 -0400

    fetchingUpdate

--- FetchTitles.js
+++ FetchTitles.js
@@ -1,6 +1,38 @@
 const API = require("./API");
 const axios = require("axios");
 
-
+async function FetchTitlesAPI(request) {
+  let ReleaseResponseData;
+  let RecordingResponseData;
+  await axios
+    .get(API.TitlesAPI.release, {
+      params: {
+        query: request,
+        ftm: "json",
+        limit: 100,
+      },
+    })
+    .then(function (response) {
+      ReleaseResponseData = JSON.parse(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  await axios
+    .get(API.TitlesAPI.recording, {
+      params: {
+        query: request,
+        ftm: "json",
+        limit: 100,
+      },
+    })
+    .then(function (response) {
+      RecordingResponseData = JSON.parse(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return { ReleaseResponseData, RecordingResponseData };
+}
 
 module.exports = { FetchTitlesAPI };
