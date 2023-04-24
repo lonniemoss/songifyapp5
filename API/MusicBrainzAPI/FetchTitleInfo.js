33f3b0f778c451c6ee99e80205dfb75b9a3df10c
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2023-04-24 15:49:35 -0400

    fetchingFromMusicBrainz

--- API/MusicBrainzAPI/FetchTitleInfo.js
+++ API/MusicBrainzAPI/FetchTitleInfo.js
@@ -3,19 +3,7 @@ const axios = require("axios");
 
 async function FetchTitleInfoAPI(titleID) {
   let responseData;
-  await axios
-    .get(API.TitlesAPI.recording + `${titleID}?inc=artist-credits`, {
-      params: {
-        ftm: "json",
-      },
-    })
-    .then(function (response) {
-      responseData = JSON.parse(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      responseData = error;
-    });
-  await axios
+  
     .get(API.TitlesAPI.release + `${titleID}?inc=artist-credits+labels`, {
       params: {
         ftm: "json",
